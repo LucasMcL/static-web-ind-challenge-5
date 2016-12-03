4b0400b7b76a7b2a9797889d460d412f4f809ef5
LucasMcL <LMcLaughlin09@gmail.com>
2016-12-03 17:48:15 -0600

    completed challenge

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 // Write a Fibonacci number generator that outputs the numbers
 // in the series that are less than 500.
+
+fibArray = [1, 1];
+console.log(fibArray[0])
+
+while(fibArray[fibArray.length - 1] < 500) {
+  var n = fibArray.length;
+  var newNum = fibArray[n-1] + fibArray[n-2];
+  console.log(fibArray[n-1])
+  fibArray.push(newNum);
+}
